f371298a34314f4f163ef3a7f820cb7d492d340e
na-ya <trom363blw@gmail.com>
2020-02-18 08:24:14 +0530

    [update]

--- jquery/drawer.js
+++ jquery/drawer.js
@@ -7,11 +7,13 @@ $(function() {
   function menuOpen() {
     drawerMenu.addClass('open'); //ドロワーメニューを表示
     overlay.fadeIn();
+    // $('body').addClass('overlay');
     closeBtn.fadeIn();
   }
   function menuClose() {
     drawerMenu.removeClass('open'); //ドロワーメニューを非表示
     overlay.fadeOut();
+    // $('body').removeClass('overlay');
     closeBtn.fadeOut('fast');
   }
 
@@ -24,5 +26,31 @@ $(function() {
   overlay.on('click', function() { //Closeボタン以外(背景)クリックでもドロワーメニューを非表示にさせる
     menuClose();
   });
+
+  // ↓オーバーレイ部分のクリックでもドロワーメニューを非表示にしようとしたがうまく行かず…
+  
+  // $('.overlay').on('click', function() {
+  //   menuClose();
+  // });
+  // $('body').hasClass('overlay').removeClass('overlay');
+  // $('body').on('click', function() {
+  //   if($('body').hasClass('overlay')) {
+  //     menuClose();
+  //   }
+  // });
+  // $('body').on('click', function() {
+  //   if($('body').hasClass('overlay')) {
+  //     menuClose();
+  //   } else {
+  //     openClose();
+  //   }
+  // });
+  // $('body.overlay').on('click', function() {
+  //   if ($('.drawer-menu').is(':visible')) {
+  //     $('.drawer-menu').removeClass('open');
+  //     $(this).removeClass('overlay');
+  //     $('.drawer-btn-close').fadeOut('fast');
+  //   }
+  // });
 
 });
